b3e62de9942fa6854a95ea5985ba2195151e0128
David Alvarez <daveyalvarez03@gmail.com>
2022-07-18 17:06:57 -0400

    continued cleanup, fix built icons

--- template-app/webpack/webpack.common.js
+++ template-app/webpack/webpack.common.js
@@ -1,23 +1,17 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const InterpolateHtmlPlugin = require('interpolate-html-plugin')
+const CopyPlugin = require('copy-webpack-plugin')
 
 module.exports = {
-  entry: path.resolve(__dirname, '..', './src/index.tsx'),
-  resolve: {
-    modules: ['node_modules'],
-    extensions: ['.tsx', '.ts', '.js'],
+  entry: {
+    bundle: path.resolve(__dirname, '..', './src/index.tsx'),
   },
 
   output: {
     path: path.resolve(__dirname, '..', './build'),
-    filename: 'bundle.js',
-  },
-
-  devServer: {
-    // static: {
-    //   directory: path.join(__dirname, 'dist/'),
-    // },
-    historyApiFallback: true,
+    filename: '[name].[contenthash].js',
+    clean: true,
   },
 
   module: {
@@ -25,18 +19,17 @@ module.exports = {
       {
         test: /\.(ts|js)x?$/,
         exclude: /node_modules/,
-        use: [
-          {
-            loader: 'babel-loader',
-          },
-        ],
+        use: {
+          loader: 'babel-loader',
+        },
       },
       {
-        test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
+        test: /\.(ico|gif|png|jpg|jpeg)$/i,
+        exclude: /public/,
         type: 'asset/resource',
       },
       {
-        test: /\.(woff(2)?|eot|ttf|otf|svg|)$/,
+        test: /\.(woff(2)?|eot|ttf|otf|svg|)$/i,
         type: 'asset/inline',
       },
     ],
@@ -46,6 +39,18 @@ module.exports = {
     new HtmlWebpackPlugin({
       template: path.resolve(__dirname, '..', './src/index.html'),
     }),
+    new InterpolateHtmlPlugin({
+      ASSETS_URL: 'assets',
+    }),
+    new CopyPlugin({
+      patterns: [{ from: 'public', to: 'assets' }],
+    }),
   ],
+
+  resolve: {
+    modules: ['node_modules'],
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+
   stats: 'errors-only',
 }
